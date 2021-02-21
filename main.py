7abad931068487882bd6ce03d9039bc3e00478c7
kkendall1221 <79422007+kkendall1221@users.noreply.github.com>
2021-02-21 13:44:20 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Kaleb Kendall
+#2020
+
+if __name__ == '__main__':
+    for i in range(100):
+
+        if i%15 ==0:
+            print("fizzbuzz")
+
+        elif i % 3 == 0:
+            print("fizz")
+
+        elif i%5 == 0:
+            print ("buzz")
+
+
+
+myname= "kaleb Kendall"
+mylastName = " Kendall"
+
+print(myname)
+
+
+
+
